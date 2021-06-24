8ea2734e6ab7b86beb11cac2f45a6a37e99cd07e
Yeeun <22000546@handong.edu>
2021-06-24 22:24:00 +0900

    Main structure

--- app/src/main/java/graphic/editor/GraphicEditor.java
+++ app/src/main/java/graphic/editor/GraphicEditor.java
@@ -1,15 +1,9 @@
 package graphic.editor;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JMenu;
-import javax.swing.JToolBar;
-import java.awt.Canvas;
-import java.awt.Color;
-import javax.swing.JButton;
+import javax.swing.*;
+import graphic.editor.tools.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class GraphicEditor {
 
@@ -47,10 +41,12 @@ public class GraphicEditor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		/*
 		Canvas canvas = new Canvas();
 		canvas.setBounds(0, 0, 1060, 650);
 		canvas.setBackground(Color.WHITE);
 		frame.getContentPane().add(canvas);	
+		
 		
 		/*
 		 * Menu Bar
@@ -105,34 +101,44 @@ public class GraphicEditor {
 		 * Tool Bar
 		 */
 		
-		JButton toolPen = new JButton("펜");
+		JButton toolPen = new JButton("");
 		toolPen.setBounds(1070, 20, 117, 40);
-		toolPen.setBackground(Color.DARK_GRAY);
+		toolPen.setIcon(new ImageIcon("./images/pen24.png"));
 		frame.getContentPane().add(toolPen);
+		toolPen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		
-		JButton toolShape = new JButton("도형");
+		JButton toolShape = new JButton("");
 		toolShape.setBounds(1070, 70, 117, 40);
 		toolShape.setBackground(Color.DARK_GRAY);
+		toolShape.setIcon(new ImageIcon("./images/shape24.png"));
 		frame.getContentPane().add(toolShape);
 		
-		JButton toolLine = new JButton("선");
+		JButton toolLine = new JButton("");
 		toolLine.setBounds(1070, 120, 117, 40);
 		toolLine.setBackground(Color.DARK_GRAY);
+		toolLine.setIcon(new ImageIcon("./images/line24.png"));
 		frame.getContentPane().add(toolLine);
 		
-		JButton toolEraser = new JButton("지우개");
+		JButton toolEraser = new JButton("");
 		toolEraser.setBounds(1070, 170, 117, 40);
 		toolEraser.setBackground(Color.DARK_GRAY);
+		toolEraser.setIcon(new ImageIcon("./images/eraser24.png"));
 		frame.getContentPane().add(toolEraser);
 		
-		JButton toolText = new JButton("텍스트");
+		JButton toolText = new JButton("");
 		toolText.setBounds(1070, 220, 117, 40);
 		toolText.setBackground(Color.DARK_GRAY);
+		toolText.setIcon(new ImageIcon("./images/text24.png"));
 		frame.getContentPane().add(toolText);
 		
-		JButton toolPattern = new JButton("패턴");
+		JButton toolPattern = new JButton("");
 		toolPattern.setBounds(1070, 270, 117, 40);
 		toolPattern.setBackground(Color.DARK_GRAY);
+		toolPattern.setIcon(new ImageIcon("./images/pattern24.png"));
 		frame.getContentPane().add(toolPattern);
 		
 		Canvas canvasToolBar = new Canvas();
@@ -143,6 +149,23 @@ public class GraphicEditor {
 		/**
 		 * Tool Setting Section
 		 */
+		
+		JButton selectStroke = new JButton("굵기");
+		selectStroke.setBounds(1070, 350, 117, 40);
+		frame.getContentPane().add(selectStroke);
+		
+		JButton selectColor = new JButton("색상");
+		selectColor.setBounds(1070, 400, 117, 40);
+		selectColor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Color color = JColorChooser.showDialog(null, "Color Menu", Color.black);
+			}
+		});
+		frame.getContentPane().add(selectColor);
+		
+		JButton selectStyle = new JButton("스타일");
+		selectStyle.setBounds(1070, 450, 117, 40);
+		frame.getContentPane().add(selectStyle);
 		
 		Canvas canvasToolSettingSection = new Canvas();
 		canvasToolSettingSection.setBackground(Color.DARK_GRAY);
